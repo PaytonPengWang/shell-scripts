5d3897b9068b59d4509b700677ca4dfe1d85cac1
payton <18991922353@163.com>
2018-04-02 22:03:00 +0800

    add mail server

--- testBootSpace.sh
+++ testBootSpace.sh
@@ -4,7 +4,7 @@
 bootspace=`df -Th | grep 'boot' | awk '{print $6}' | cut -d '%' -f1`
 
 if [ bootspace -gt 14 ] then
-	echo 'boot space greater than 14%'
+	echo 'boot space greater than 14%' | mail -s 'boot space warning!!!' root@localhost
 else 
 	echo 'boot space less than 14%'
 fi
